234ecbf5aaeebff006482b3c954d86ed992fd90f
Emmanuel <www.proxynova95@gmail.com>
2025-06-24 12:33:05 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+// 1. Declare customerName globally and initialize to 'bob'
+let customerName = 'bob';
+
+function returnCustomerName() {
+  return customerName;
+}
+
+// 2. Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3. Declare bestCustomer (undefined initially)
+let bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// 4. Overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// 5. Declare leastFavoriteCustomer as constant
+const leastFavoriteCustomer = 'alice';
+
+function changeLeastFavoriteCustomer() {
+  // This line will cause an error because leastFavoriteCustomer is const
+  leastFavoriteCustomer = 'bob';
+}
